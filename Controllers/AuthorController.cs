df0d7c63fd0f55b96db68218198461a86485cb33
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-02-21 01:03:36 +0400

    refactor: Author Update and Delete Actions

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -13,6 +13,7 @@ using Web_API_Relations.Data.Entity;
 namespace Web_API_Relations.Controllers
 {
     [Route("[controller]")]
+    //[ApiController]
     public class AuthorController : Controller
     {
         private readonly Context _context;
@@ -31,7 +32,7 @@ namespace Web_API_Relations.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult Get([FromBody]int id)
+        public IActionResult Get([FromBody] int id)
         {
             if (id == null) return NotFound();
             Author dbAuthor = _context.Authors.FirstOrDefault(a => a.Id == id);
@@ -52,7 +53,7 @@ namespace Web_API_Relations.Controllers
             Author newAuthor = new Author
             {
                 Name = authorCreateDto.Name,
-                LastName= authorCreateDto.LastName,
+                LastName = authorCreateDto.LastName,
                 Age = authorCreateDto.Age,
             };
             _context.Authors.Add(newAuthor);
@@ -62,13 +63,13 @@ namespace Web_API_Relations.Controllers
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public IActionResult Update([FromForm]int id,Author author)
+        public IActionResult Update(int id, [FromBody]AuthorCreateDto author)
         {
             Author dbAuthor = _context.Authors.Where(a => a.Id == id).FirstOrDefault();
             if (dbAuthor == null) NotFound();
-            dbAuthor.Name = author.Name;
-            dbAuthor.LastName = author.LastName;
-            dbAuthor.Age = author.Age;
+            dbAuthor.Name = author.Name??dbAuthor.Name;
+            dbAuthor.LastName = author.LastName??dbAuthor.LastName;
+            dbAuthor.Age = author.Age==0?dbAuthor.Age:author.Age;
             _context.SaveChanges();
             return StatusCode(200);
 
@@ -76,14 +77,12 @@ namespace Web_API_Relations.Controllers
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public IActionResult Delete([FromBody]int id)
+        public IActionResult Delete(int id)
         {
             if (id == null) return NotFound();
             Author dbAuthor = _context.Authors.FirstOrDefault(a => a.Id == id);
             if (dbAuthor == null) return NotFound();
 
-            var bookRelateds =  _context.BookRelateds.FirstOrDefault(b => b.AuthorId == dbAuthor.Id);
-            _context.BookRelateds.Remove(bookRelateds);
             _context.Authors.Remove(dbAuthor);
             _context.SaveChanges();
             return StatusCode(202);
