c191b7f1f340b25ac8238d9bef50afeea6326c0c
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-02-20 15:09:58 +0400

    feat: Author CRUD

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web_API_Relations.Data.DAL;
+using Web_API_Relations.Data.Entity;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Web_API_Relations.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthorController : Controller
+    {
+        private readonly Context _context;
+
+        public AuthorController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public List<Author> Get()
+        {
+            return _context.Authors.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            if (id == null) return NotFound();
+            Author dbAuthor = _context.Authors.FirstOrDefault(a => a.Id == id);
+            if (dbAuthor == null) return StatusCode(404);
+
+            return StatusCode(200, dbAuthor);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Create(Author author)
+        {
+            bool isExist = _context.Authors.Any(a => a.Name.ToLower().Trim() == author.Name.ToLower().Trim());
+            if (isExist)
+            {
+                return StatusCode(401);
+            }
+            Author newAuthor = new Author
+            {
+                Name = author.Name,
+                LastName=author.LastName,
+                Age =author.Age,
+                
+            };
+            _context.Authors.Add(author);
+            _context.SaveChanges();
+            return StatusCode(201);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Update(int id,Author author)
+        {
+            Author dbAuthor = _context.Authors.Where(a => a.Id == id).FirstOrDefault();
+            if (dbAuthor == null) NotFound();
+            dbAuthor.Name = author.Name;
+            dbAuthor.LastName = author.LastName;
+            dbAuthor.Age = author.Age;
+            _context.SaveChanges();
+            return StatusCode(200);
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (id == null) return NotFound();
+            Author dbAuthor = _context.Authors.FirstOrDefault(a => a.Id == id);
+            if (dbAuthor == null) return NotFound();
+            _context.Authors.Remove(dbAuthor);
+            _context.SaveChanges();
+            return StatusCode(202);
+        }
+    }
+}
--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Web_API_Relations.Data.DAL;
+using Web_API_Relations.Data.Entity;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,31 +13,57 @@ namespace Web_API_Relations.Controllers
     [Route("api/[controller]")]
     public class BookController : ControllerBase
     {
+
+        private readonly Context _context;
+
+        public BookController(Context context)
+        {
+            _context = context;
+        }
+
+
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public List<Book> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _context.Books.ToList();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            if (id == null) return NotFound();
+            Book dbBook = _context.Books.FirstOrDefault(b => b.Id == id);
+            if(dbBook==null) return StatusCode(404);
+
+            return StatusCode(200, dbBook);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Create(Book book)
         {
+            bool isExist = _context.Books.Any(b => b.Name.ToLower().Trim() == book.Name.ToLower().Trim());
+            if (isExist)
+            {
+                return StatusCode(401);
+            }
+            Book newBook = new Book
+            {
+                Name = book.Name,
+                AuthorId = book.AuthorId,
+            };
+            _context.Books.Add(book);
+            _context.SaveChanges();
+            return StatusCode(201);
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public IActionResult Update(int id,)
+        //{
+        //}
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
